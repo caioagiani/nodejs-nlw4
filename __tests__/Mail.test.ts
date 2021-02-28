b49a59803739e51f11c27bc2a82f4f96daeca926
caioagiani <agianicaio@gmail.com>
2021-02-28 03:05:19 -0300

    test: refactored coverage sendmail for all conditions

--- __tests__/Mail.test.ts
+++ __tests__/Mail.test.ts
@@ -14,12 +14,45 @@ describe('Mail', () => {
     expect(body).toHaveProperty('id');
   });
 
-  it('should return an error when sending email with invalid parameters', async () => {
-    const { status } = await requests.sendMail({
-      email: 'invalid',
-      survey_id: 'invalid',
+  it('should return an error when sending email with invalid uuid', async () => {
+    const userResponse = await requests.users({
+      email: 'uuid@gmail.com',
+      name: 'invalid uuid',
+    });
+
+    const { body, status } = await requests.sendMail({
+      email: userResponse.body.email,
+      survey_id: 'invalid_uuid',
     });
 
     expect(status).toBe(400);
+    expect(body.error).toBe('UUID is invalid.');
+  });
+
+  it('should return an error when sending email with invalid email', async () => {
+    const surveyResponse = await requests.surveys();
+
+    const { body, status } = await requests.sendMail({
+      email: 'invalid_email',
+      survey_id: surveyResponse.body.id,
+    });
+
+    expect(status).toBe(400);
+    expect(body.error).toBe('User does not exists.');
+  });
+
+  it('should return an error when sending email with invalid survey', async () => {
+    const userResponse = await requests.users({
+      email: 'survey@gmail.com',
+      name: 'invalid survey',
+    });
+
+    const { body, status } = await requests.sendMail({
+      email: userResponse.body.email,
+      survey_id: '4a3a83af-b60c-4c44-ab78-4bcc1eb16512',
+    });
+
+    expect(status).toBe(400);
+    expect(body.error).toBe('Survey does not exists.');
   });
 });
