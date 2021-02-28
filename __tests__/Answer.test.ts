9427bb26bd8948d97b6ed51ce631bfd9c96b86ea
caioagiani <agianicaio@gmail.com>
2021-02-28 03:14:29 -0300

    feat: coverage uuid does not exists

--- __tests__/Answer.test.ts
+++ __tests__/Answer.test.ts
@@ -18,8 +18,19 @@ describe('Answer', () => {
   });
 
   it('should return an error when receive survey note', async () => {
-    const { status } = await requests.answer(7, 'invalid_id');
+    const { body, status } = await requests.answer(7, 'invalid_id');
 
     expect(status).toBe(400);
+    expect(body.error).toBe('UUID is invalid.');
+  });
+
+  it('should return an error with uuid that does not exists', async () => {
+    const { body, status } = await requests.answer(
+      7,
+      '4a3a83af-b60c-4c44-ab78-4bcc1eb16512',
+    );
+
+    expect(status).toBe(400);
+    expect(body.error).toBe('SurveyUser does not exists.');
   });
 });
