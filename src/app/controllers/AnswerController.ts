07e1324028647410b33b999c536810617bc3d644
caioagiani <agianicaio@gmail.com>
2021-02-28 01:52:33 -0300

    feat: added validation uuid

--- src/app/controllers/AnswerController.ts
+++ src/app/controllers/AnswerController.ts
@@ -1,11 +1,18 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
+import { UUIDv4 } from 'uuid-v4-validator';
 import { SurveysUsersRepository } from '../repositories/SurveysUsersRepository';
 
 class AnswerController {
   async execute(req: Request, res: Response) {
     const { value } = req.params;
     const { u } = req.query;
+
+    if (!UUIDv4.validate(String(u))) {
+      return res.status(400).json({
+        error: 'UUID is invalid.',
+      });
+    }
 
     const surveysUsersRepository = getCustomRepository(SurveysUsersRepository);
 
